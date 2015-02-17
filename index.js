497c5bc59d242783275e6baff35207db00ecf711
Josh Black <joshuatblack@ufl.edu>
2015-02-17 13:58:22 -0500

    Adding react@0.13.0-beta.2 support

--- index.js
+++ index.js
@@ -2,11 +2,17 @@
 
 import React from 'react';
 
-const App = React.createClass({
+// const App = React.createClass({
+//     render () {
+//         return <div>Hello World!!</div>;
+//     }
+// });
+
+class App extends React.Component {
     render () {
-        return (<div>Hello World!!</div>);
+        return (<div>Hello World!</div>);
     }
-});
+}
 
 
 React.render(<App />, document.body);
